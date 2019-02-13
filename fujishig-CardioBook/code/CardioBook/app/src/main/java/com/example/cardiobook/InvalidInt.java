64ceab93dd2df87bfe58042f647af93d543f14e2
kyle <fujishig@ualberta.ca>
2019-02-13 16:13:08 -0700

    master

--- fujishig-CardioBook/code/CardioBook/app/src/main/java/com/example/cardiobook/InvalidInt.java
+++
@@ -1,9 +0,0 @@
-package com.example.cardiobook;
-
-/**<p>Calls a runtime exception when invoked</p>
- * @author kyle
- * @version 1.0
- * @see RuntimeException
- */
-public class InvalidInt extends RuntimeException {
-}
